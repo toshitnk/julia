09b7290efe79032f4f6ed21b16365ad34989987e
{toshitnk} <{toshiya0417tanaka@gmail.com}>
2021-10-17 19:39:38 +0900

    compare stirling's formula

--- physics/statmech/factorial_approx.jl
+++ physics/statmech/factorial_approx.jl
@@ -1,7 +1,9 @@
 using Plots
 
-N = 1:20
+N = 1:100
 approx = N .* (log.(N) .- 1)
-plot(N, log.(factorial.(big.(N))), label=raw"$\log N!$", legend=:topleft)
-plot!(N, approx, label=raw"$N(\log N -1)$")
+approx2 = 0.5 .* log.(2 * π .* N) .+ N .* (log.(N) .- 1)
+plot(N, log.(factorial.(big.(N))),lc = :black,  label=raw"$\log N!$", legend=:topleft)
+plot!(N, approx, label=raw"$N(\log N -1)$",lc = :black, ls= :dot)
+# plot!(N, approx2, label=raw"$\log(\sqrt{2\pi n}(n/e)^n)$", ls = :dash, lc=:black)
 xlabel!(raw"$N$")
